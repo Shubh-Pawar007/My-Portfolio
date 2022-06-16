93c0efcf9327dbf32d163475c62e559e803889c5
Shubh-Pawar007 <shubhampawar242767@gmail.com>
2022-06-16 14:40:15 +0530

    minor update

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Shubham Pawar</span>
+            Hi Everyone, I am <span className="purple">Shubham Pawar </span>
             from <span className="purple"> Pune, India.</span>
             <br />I am an enthusiastic web developer, like to build beautiful websites.
             <br />
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,7 +8,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =  "https://github.com/SVickyPawar/portfolio/master/src/Assets/Vivek_Pawar_resume.pdf";
+const resumeLink =  "https://github.com/Shubh-Pawar007/My-Portfolio/blob/main/src/Assets/Shubham_Pawar_fw16_054.pdf";
 
 
 function ResumeNew() {
@@ -39,18 +39,7 @@ function ResumeNew() {
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
-{/* 
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row> */}
+
       </Container>
     </div>
   );
