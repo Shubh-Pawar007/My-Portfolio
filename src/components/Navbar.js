6907d9107f867559ea73c03763a8e3114d3c4d2d
Shubham Pawar <99669405+Shubh-Pawar007@users.noreply.github.com>
2022-08-21 22:58:38 +0530

    pointer update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -52,13 +52,13 @@ function NavBar() {
         </Navbar.Toggle>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto" >
-            <Nav.Item>
+            <Nav.Item style={{cursor:'pointer'}}>
               <Nav.Link as={Link} to="home" onClick={() => updateExpanded(false)}>
                 <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
+            <Nav.Item style={{cursor:'pointer'}}>
               <Nav.Link
                 as={Link}
                 to="about"
@@ -68,7 +68,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
             
-            <Nav.Item>
+            <Nav.Item style={{cursor:'pointer'}}>
               <Nav.Link
                 as={Link}
                 to="skills"
@@ -81,7 +81,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
+            <Nav.Item style={{cursor:'pointer'}}>
               <Nav.Link
                 as={Link}
                 to="projects"
@@ -94,7 +94,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
+            <Nav.Item style={{cursor:'pointer'}}>
               <Nav.Link
                 as={Link}
                 to="resume"
@@ -104,7 +104,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
+            <Nav.Item style={{cursor:'pointer'}}>
               <Nav.Link
                 as={Link}
                 to="contact"
